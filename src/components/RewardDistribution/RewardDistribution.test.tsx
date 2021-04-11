555808d26bf5cbe54500130d75db4e15bed59d8f
Johannes Hüsers <johannes.huesers@teamecho.at>
2021-04-11 19:26:55 +0200

    Add more tests for RewardDistribution

--- src/components/RewardDistribution/RewardDistribution.test.tsx
+++ src/components/RewardDistribution/RewardDistribution.test.tsx
@@ -3,18 +3,25 @@ import React from 'react';
 import RewardDistribution from './RewardDistribution';
 import { cleanup, render } from '@testing-library/react';
 
-afterEach(cleanup);
+describe('<RewardDistribution>', () => {
+  afterEach(cleanup);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RewardDistribution/>, div);
-});
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<RewardDistribution/>, div);
+  });
 
-it('renders correctly', () => {
-  render(<RewardDistribution/>);
-});
+  it('renders correctly', () => {
+    render(<RewardDistribution/>);
+  });
 
-it('matches snapshot', () => {
-  const { asFragment } = render(<RewardDistribution/>);
-  expect(asFragment()).toMatchSnapshot();
+  it('includes mining reward column', () => {
+    const rewardDistribution = render(<RewardDistribution/>);
+    expect(rewardDistribution.getByText('Mining Reward')).toBeInTheDocument();
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<RewardDistribution/>);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
